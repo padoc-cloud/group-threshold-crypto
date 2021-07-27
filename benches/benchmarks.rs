09ec8a6d60a76aab4f6af3a677658647ad334e70
padoc-cloud <franlebaocbrahves47@gmail.com>
2021-07-27 13:58:38 +0100

    benchmark across msg num and size

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -7,7 +7,7 @@ pub fn bench_decryption(c: &mut Criterion) {
 
     const NUM_OF_TX: usize = 1000;
 
-    fn share_combine_bench(num_msg: usize, num_shares: usize, num_entities: usize) -> impl Fn() {
+    fn share_combine_bench(num_msg: usize, num_shares: usize, num_entities: usize, msg_size: usize) -> impl Fn() {
         let rng = &mut rand::rngs::StdRng::seed_from_u64(0);
 
         type E = ark_bls12_381::Bls12_381;
@@ -15,12 +15,17 @@ pub fn bench_decryption(c: &mut Criterion) {
 
         let (pubkey, _, contexts) = setup::<E>(threshold, num_shares, num_entities);
 
-        let mut messages: Vec<[u8; NUM_OF_TX]> = vec![];
+        // let mut messages: Vec<[u8; NUM_OF_TX]> = vec![];
+        let mut messages: Vec<Vec<u8>> = vec![];
         let mut ciphertexts: Vec<Ciphertext<E>> = vec![];
         let mut dec_shares: Vec<Vec<DecryptionShare<E>>> = Vec::with_capacity(ciphertexts.len());
         for j in 0..num_msg {
-            let mut msg: [u8; NUM_OF_TX] = [0u8; NUM_OF_TX];
-            rng.fill_bytes(&mut msg);
+            // let mut msg: [u8; NUM_OF_TX] = [0u8; NUM_OF_TX];
+            let mut msg: Vec<u8> = vec![];
+            for _ in 0..msg_size {
+                msg.push(0u8);
+            }
+            rng.fill_bytes(&mut msg[..]);
             messages.push(msg.clone());
 
             ciphertexts.push(encrypt::<_, E>(&messages[j], pubkey, rng));
@@ -48,7 +53,7 @@ pub fn bench_decryption(c: &mut Criterion) {
         share_combine_prepared
     }
 
-    fn block_propose_bench(num_msg: usize, num_shares: usize, num_entities: usize) -> impl Fn() {
+    fn block_propose_bench(num_msg: usize, num_shares: usize, num_entities: usize, msg_size: usize) -> impl Fn() {
         let rng = &mut rand::rngs::StdRng::seed_from_u64(0);
 
         type E = ark_bls12_381::Bls12_381;
@@ -56,11 +61,16 @@ pub fn bench_decryption(c: &mut Criterion) {
 
         let (pubkey, _, contexts) = setup::<E>(threshold, num_shares, num_entities);
 
-        let mut messages: Vec<[u8; NUM_OF_TX]> = vec![];
+        // let mut messages: Vec<[u8; NUM_OF_TX]> = vec![];
+        let mut messages: Vec<Vec<u8>> = vec![];
         let mut ciphertexts: Vec<Ciphertext<E>> = vec![];
         let mut dec_shares: Vec<Vec<DecryptionShare<E>>> = Vec::with_capacity(ciphertexts.len());
         for j in 0..num_msg {
-            let mut msg: [u8; NUM_OF_TX] = [0u8; NUM_OF_TX];
+            // let mut msg: [u8; NUM_OF_TX] = [0u8; NUM_OF_TX];
+            let mut msg: Vec<u8> = vec![];
+            for _ in 0..msg_size {
+                msg.push(0u8);
+            }
             rng.fill_bytes(&mut msg);
             messages.push(msg.clone());
 
@@ -95,17 +105,34 @@ pub fn bench_decryption(c: &mut Criterion) {
     let mut group = c.benchmark_group("TPKE");
     group.sample_size(10);
 
-    let a = share_combine_bench(100, 8192, 150);
-    group.measurement_time(core::time::Duration::new(30, 0));
-    group.bench_function("share_combine: threshold 8192*2/3 - #msg 100", |b| {
-        b.iter(|| a())
-    });
+    // let a = share_combine_bench(100, 8192, 150);
+    // group.measurement_time(core::time::Duration::new(30, 0));
+    // group.bench_function("share_combine: threshold 8192*2/3 - #msg 100", |b| {
+    //     b.iter(|| a())
+    // });
 
-    let a = block_propose_bench(100, 8192, 150);
-    group.measurement_time(core::time::Duration::new(30, 0));
-    group.bench_function("block_propose: threshold 8192*2/3 - #msg 100", |b| {
-        b.iter(|| a())
-    });
+    // let a = block_propose_bench(100, 8192, 150);
+    // group.measurement_time(core::time::Duration::new(30, 0));
+    // group.bench_function("block_propose: threshold 8192*2/3 - #msg 100", |b| {
+    //     b.iter(|| a())
+    // });
+
+
+    for msg_num in vec![10, 100, 1000].iter() {
+        for msg_size in vec![100, 1000, 10000].iter() {
+            let a = share_combine_bench(*msg_num, 8192, 150, *msg_size);
+            group.measurement_time(core::time::Duration::new(30, 0));
+            group.bench_function(format!("share_combine: threshold 8192*2/3 - #msg {} - msg-size = {} bytes", msg_num, msg_size), |b| {
+                b.iter(|| a())
+            });
+
+            let a = block_propose_bench(*msg_num, 8192, 150, *msg_size);
+            group.measurement_time(core::time::Duration::new(30, 0));
+            group.bench_function(format!("block_propose: threshold 8192*2/3 - #msg {} - msg-size = {} bytes", msg_num, msg_size), |b| {
+                b.iter(|| a())
+            });
+        }
+    }
 }
 
 criterion_group!(benches, bench_decryption);
